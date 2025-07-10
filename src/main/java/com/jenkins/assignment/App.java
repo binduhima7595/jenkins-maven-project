5fe181ea5fbb184d31b6a916b574956b9727b8ce
binduhima7595 <binduhima7595@gmail.com>
2025-07-10 13:03:29 -0500

    add the helloworld java

--- src/main/java/com/jenkins/assignment/App.java
+++ src/main/java/com/jenkins/assignment/App.java
@@ -1,13 +1,7 @@
 package com.jenkins.assignment;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins Maven Project!");
     }
 }
